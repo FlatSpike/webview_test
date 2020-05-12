b4bfaa64adf96cff79f5ee18624578b3ca38b7d1
Rodion <shatalov.rodion@gmail.com>
2020-05-12 21:12:43 +0300

    Make yahoo dialog's background transparent

--- app/src/main/java/ru/flatspike/test/dialogs/YahooPromotionDialog.kt
+++ app/src/main/java/ru/flatspike/test/dialogs/YahooPromotionDialog.kt
@@ -1,6 +1,8 @@
 package ru.flatspike.test.dialogs
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -25,6 +27,9 @@ class YahooPromotionDialog : DialogFragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        // make dialog window transparent (that the easiest way)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
         binding.root.setOnClickListener {
             startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://yahoo.com")))
